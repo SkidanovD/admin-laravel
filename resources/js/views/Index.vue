1d74c32c079f1e6974d11ab92f97146074f6ec99
TixonRyzhkov <tixon24@gmail.com>
2022-01-26 11:01:53 +0200

    first commit

--- resources/js/views/Index.vue
+++ resources/js/views/Index.vue
@@ -1,7 +1,7 @@
 <template>
    <main class="site-main home-main">
       <div class="site-main-wrapper home-main-wrapper">
-         <h1>Index</h1>
+         <h1>Index 2</h1>
 
       </div>
    </main>
